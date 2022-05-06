edbc78f23d74ad122757c00f0a397796dbe89079
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-05-06 12:44:46 +0100

    spinner

--- tourism-guide/src/pages/AllPosts.js
+++ tourism-guide/src/pages/AllPosts.js
@@ -1,45 +1,41 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import PostList from "../components/posts/PostList";
-
-const DUMMY_DATA = [
-    {
-        id: "m1",
-        title: "This is a first meetup",
-        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-        address: "Meetupstreet 5, 12345 Meetup City",
-        description:
-            "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-    },
-    {
-        id: "m2",
-        title: "This is a second meetup",
-        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-        address: "Meetupstreet 5, 12345 Meetup City",
-        description:
-            "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-    },
-];
+import LoadingSpinner from "./spinner/LoadingSpinner";
 
 export default function AllPostsPage() {
     const [isLoading, setIsLoading] = useState(true);
     const [loadedPosts, setLoadedposts] = useState([]);
 
-    fetch(
-        "https://react-app-tourist-guide-default-rtdb.firebaseio.com/posts.json"
-    )
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            setIsLoading(false);
-            setLoadedposts(data);
-        });
+    useEffect(() => {
+        setIsLoading(true);
+        fetch(
+            "https://react-app-tourist-guide-default-rtdb.firebaseio.com/posts.json"
+        )
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                const meetups = [];
+
+                for (const key in data) {
+                    const meetup = {
+                        id: key,
+                        ...data[key],
+                    };
+
+                    meetups.push(meetup);
+                }
+
+                setIsLoading(false);
+                setLoadedposts(meetups);
+            });
+    }, []);
 
     if (isLoading) {
         return (
             <section>
-                <p> Loading... </p>
+                <LoadingSpinner />
             </section>
         );
     }
